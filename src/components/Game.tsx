3d056514d808f53b435d14dfedb92db61ac1ecb6
Massoud Valipoor <masval444@msn.com>
2023-08-11 15:04:45 +0200

    add click to start

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Phaser from "phaser";
 import config from "@/app/config";
 
 const Game: React.FC = () => {
   const gameRef = useRef<Phaser.Game | null>(null);
+  const [startGame, setStartGame] = useState(false);
 
   useEffect(() => {
-    const game = new Phaser.Game(config);
-    gameRef.current = game;
+    const initializeGame = () => {
+      const game = new Phaser.Game(config);
+      gameRef.current = game;
+    };
+
+    const handleUserGesture = async () => {
+      initializeGame();
+      setStartGame(true);
+      window.removeEventListener("click", handleUserGesture);
+    };
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("click", handleUserGesture);
+    }
 
     return () => {
       if (gameRef.current) {
@@ -19,7 +32,7 @@ const Game: React.FC = () => {
     };
   }, []);
 
-  return <div />;
+  return <>{startGame ? <div /> : "click to start"}</>;
 };
 
 export default Game;
