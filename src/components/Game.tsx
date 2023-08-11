64a860e69b596ebfb54a9aea4a29bb52673d90fc
Massoud Valipoor <masval444@msn.com>
2023-08-11 14:54:43 +0200

    clean up in 'Game' component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,29 +1,15 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
+import Phaser from "phaser";
 import config from "@/app/config";
 
 const Game: React.FC = () => {
   const gameRef = useRef<Phaser.Game | null>(null);
 
   useEffect(() => {
-    const initializeGame = async () => {
-      const Phaser = await import("phaser");
-
-      const game = new Phaser.Game(config);
-      gameRef.current = game;
-    };
-
-    // const handleUserGesture = async () => {
-    //   await initializeGame();
-    //   window.removeEventListener("click", handleUserGesture);
-    // };
-
-    // if (typeof window !== "undefined") {
-    //   window.addEventListener("click", handleUserGesture);
-    // }
-
-    initializeGame();
+    const game = new Phaser.Game(config);
+    gameRef.current = game;
 
     return () => {
       if (gameRef.current) {
